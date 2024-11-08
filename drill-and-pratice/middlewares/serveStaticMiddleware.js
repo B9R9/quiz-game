caf2e24324381394a9b64ac77b323105fc345c70
Baptiste Riffard <baptiste@unknown1a735f2339da.elisakoti>
2024-11-08 14:31:25 +0200

    Fix : #5 Fix path for deploy Add deno.cwd

--- drill-and-pratice/middlewares/serveStaticMiddleware.js
+++ drill-and-pratice/middlewares/serveStaticMiddleware.js
@@ -7,7 +7,7 @@ const serveStaticMiddleware = async (context, next) => {
   if (filePath.startsWith("/css") || filePath.startsWith("/js")) {
     try {
       await send(context, filePath, {
-        root: `${Deno.cwd()}/src/assets`,
+        root: `${Deno.cwd()}/assets`,
       });
     } catch (error) {
       console.error("Error serving static file:", error);
