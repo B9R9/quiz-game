413df8157d0dc1b748385f417ceccb52a81ccba9
Baptiste Riffard <baptiste@unknown4a02df750e9a.elisakoti>
2024-11-14 17:40:39 +0200

    update database connection

--- drill-and-pratice/database/database.js
+++ drill-and-pratice/database/database.js
@@ -2,36 +2,32 @@ import { neon } from "@neon/serverless";
 import { log } from "../utils/logger.js";
 
 const mode = Deno.env.get("MODE");
+log(`Mode: ${mode}`, "info", "database.js");
 
 let sql;
 
-// async function executeQuery(query, params) {
-//   let result;
-//   log(`Executing query: ${query}`, "info", "database.js");
-//   if (mode === "DEV") {
-//     result = await client.query(query, params);
-//   } else if (mode === "PROD") {
-//     result = await client.sql(query);
-//   }
-//   console.log(result);
-//   return result;
-// }
+async function initializeSQL() {
+  if (mode === "PROD") {
+    const databaseUrl = Deno.env.get("DATABASE_PROD_URL");
+    log(`Database URL: PROD DB`, "info", "database.js");
+    sql = neon(databaseUrl);
+  } else if (mode === "DEV") {
+    const databaseUrl = Deno.env.get("DATABASE_DEV_URL");
+    console.log(databaseUrl);
+    log(`Database URL: DEV DB`, "info", "database.js");
+    sql = neon(databaseUrl);
+  }
 
-if (mode === "PROD") {
-  const databaseUrl = Deno.env.get("DATABASE_PROD_URL");
-  log(`Database URL: PROD DB`, "info", "database.js");
-  sql = neon(databaseUrl);
-  log("Checking database connection...", "info", "database.js");
-  const result = await sql`SELECT ${"Database"} || ' is connected' as message`;
-  log(result[0].message, "info", "database.js");
+  // Test the connection
+  if (sql) {
+    const result =
+      await sql`SELECT ${"Database"} || ' is connected' as message`;
+    log(result[0].message, "info", "database.js");
+  } else {
+    log("Failed to initialize SQL client.", "error", "database.js");
+  }
 }
-if (mode === "DEV") {
-  const databaseUrl = Deno.env.get("DATABASE_DEV_URL");
-  log(`Database URL: DEV DB`, "info", "database.js");
-  sql = neon(databaseUrl);
-  log("Checking database connection...", "info", "database.js");
-  const result = await sql`SELECT ${"Database"} || ' is connected' as message`;
-  log(result[0].message, "info", "database.js");
-}
+
+await initializeSQL();
 
 export { sql };
