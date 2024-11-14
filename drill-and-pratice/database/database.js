748485aff590a056cd035023fd6f3d306874f68f
Baptiste Riffard <baptiste@unknown4a02df750e9a.elisakoti>
2024-11-14 22:21:46 +0200

    add sql local db

--- drill-and-pratice/database/database.js
+++ drill-and-pratice/database/database.js
@@ -1,5 +1,6 @@
 import { neon } from "@neon/serverless";
 import { log } from "../utils/logger.js";
+import postgres from "https://deno.land/x/postgresjs@v3.4.4/mod.js";
 
 const mode = Deno.env.get("MODE");
 log(`Mode: ${mode}`, "info", "database.js");
@@ -16,6 +17,15 @@ async function initializeSQL() {
     console.log(databaseUrl);
     log(`Database URL: DEV DB`, "info", "database.js");
     sql = neon(databaseUrl);
+  } else if (mode === "SQL") {
+    log(`Database URL: SQL`, "info", "database.js");
+    sql = postgres({
+      user: Deno.env.get("PG_DEV_USER"),
+      database: Deno.env.get("PG_DEV_DB"),
+      hostname: Deno.env.get("PG_DEV_HOST"),
+      password: Deno.env.get("PG_DEV_PASSWORD"),
+      port: parseInt(Deno.env.get("PG_DEV_PORT")),
+    });
   }
 
   // Test the connection
