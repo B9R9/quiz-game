76d177f69703ca29876dbdaa63aca99967557df1
Baptiste <baptiste.riffard@gmail.com>
2024-11-27 13:53:39 +0200

    test adding e2e test to worflow

--- e2e-tests/playwright.config.js
+++ e2e-tests/playwright.config.js
@@ -70,9 +70,9 @@ module.exports = defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: {
-    command: "npm run start",
-    url: "http://localhost:7777",
-    reuseExistingServer: !process.env.CI,
-  },
+  //   webServer: {
+  //     command: "npm run start",
+  //     url: "http://localhost:7777",
+  //     reuseExistingServer: !process.env.CI,
+  //   },
 });
