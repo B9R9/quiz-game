3bd576e736566042ea911ff5245eb64c388f6533
Baptiste <baptiste.riffard@gmail.com>
2024-12-04 14:10:45 +0200

    add playwright.yml

---
+++ e2e-tests/tests/utils/utils.js
@@ -0,0 +1,11 @@
+export const userProfil = {
+  email: "test@test.test",
+  username: "Joe Dassin",
+  password: "Test@4",
+};
+
+export const adminProfil = {
+  email: "admin@admin.admin",
+  username: "Admin",
+  password: "Admin@4",
+};
